058f67645a871d8ee7b531a439de15cefc4f4b81
Samantha <samantha.c.alvarenga@gmail.com>
2025-03-19 21:59:19 -0300

    add propriedades resizeMode e ajustando img no style

--- App.tsx
+++ App.tsx
@@ -12,14 +12,13 @@ export default function App() {
 
       <View style={styles.post}>
         <Text style={styles.headerPost}>Rio de Janeiro</Text>
+        
+          <Image style={styles.img}
+            source={require('./assets/image/rio-de-janeiro.jpg')}
+            resizeMode='cover'
+          />
         <Text style={styles.textPost}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut temporibus aliquid delectus nostrum adipisci est nulla nemo quaerat. Rerum magnam aut harum aliquam velit tempore perspiciatis voluptatibus animi. Eligendi, est?
         </Text>
-        <View style={styles.ImgContainer}>
-          <Image style={styles.img}
-            source={require('./assets/Rj.jpg')}
-          />
-        </View>
-
 
         {/*  <Button title="Saiba mais" color= ""></Button>   */}
         <TouchableOpacity style={styles.button}>
@@ -29,12 +28,13 @@ export default function App() {
 
       <View style={styles.post}>
         <Text style={styles.headerPost}>São Paulo</Text>
-        <Text style={styles.textPost}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut temporibus aliquid delectus nostrum adipisci est nulla nemo quaerat. Rerum magnam aut harum aliquam velit tempore perspiciatis voluptatibus animi. Eligendi, est?</Text>
-        <View style={styles.ImgContainer}>
+     
           <Image style={styles.img}
-            source={require('./assets/Sp.jpg')}
-          />
-        </View>
+            source={require('./assets/image/Sp.jpg')}
+            resizeMode='cover' 
+          />       
+        <Text style={styles.textPost}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut temporibus aliquid delectus nostrum adipisci est nulla nemo quaerat. Rerum magnam aut harum aliquam velit tempore perspiciatis voluptatibus animi. Eligendi, est?</Text>
+        
         {/* <Button title="Saiba mais"></Button> */}
         <TouchableOpacity style={styles.button}>
           <Text style={styles.buttonText}>Saiba mais</Text>
@@ -81,13 +81,14 @@ const styles = StyleSheet.create({
     backgroundColor: '#c1c1c1',
     fontSize: 20,
     textAlign: 'center',
-    paddingVertical: 12,
+    paddingVertical: 1,
     fontWeight: 'bold'
   },
 
   textPost: {    
     fontSize: 14,
-    marginHorizontal: 12
+    margin: 12
+    
   },
 
   button: {
@@ -105,27 +106,20 @@ const styles = StyleSheet.create({
     fontWeight: 'bold'
   },
 
-  ImgContainer: {
-    alignItems: 'center',      
-    backgroundColor: '#f5f5f5',
-    
-  },
-
+ 
   img: {
     borderColor: '#c1c1c1', 
-    borderWidth: 5,
-    width: 200,
-    height: 100,
-    textAlign: 'center',
-    marginTop: 10,
-    borderRadius: 8,
+    width: 'auto',
+    height: 120,
+   /*  marginTop: 10,   */
+    /* borderRadius: 8, */
   },
 
 
   footer: {
     width: '100%',
     backgroundColor: '#FF0000',
-    flex: 0.5,
+    flex: 0.25,
     justifyContent: 'center',
 
 
