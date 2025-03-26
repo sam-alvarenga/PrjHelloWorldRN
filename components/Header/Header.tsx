7940288d2f0852c4192215963613721caf6664fe
Samantha <samantha.c.alvarenga@gmail.com>
2025-03-26 20:47:42 -0300

    import FontAweson e inserindo componente

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,11 +1,14 @@
 import { Text, View} from 'react-native';
 import {headerStyles} from  './HeaderStyle';
-
+//Passo 1º Impotar a biblioteca do Font Awesome na tela que utlizará o link
+import FontAwesome from '@expo/vector-icons/FontAwesome6'
 export default function App() {
     return (
 
         <View style={headerStyles.header}>
             <Text style={headerStyles.headerText}>Só Viagens</Text>
+            {/* Inserir o componente FontAweson e suas propriedades */}
+            <FontAwesome size={30} name="plane-departure" color="white"/>
         </View>
     );
 }
--- components/Header/HeaderStyle.tsx
+++ components/Header/HeaderStyle.tsx
@@ -7,11 +7,12 @@ export const headerStyles = StyleSheet.create({
 
         /* ocupa uma parte do espaço disponível no contêiner. Como ela tem flex: 1, ela vai dividir o espaço de maneira igual com outros componentes que também têm flex: 1. */
         flex: 1,
+        flexDirection: 'row',
         backgroundColor: '#FF0000',
         justifyContent: 'center',
         alignItems: 'center',
-        paddingTop: 22
-
+        paddingTop: 22,
+        gap: 16,
     },
 
     headerText: {
